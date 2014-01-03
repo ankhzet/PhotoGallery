5b81015884c6df4f1dbb7286024864982d260e6a
ankhzet <ankhzet@gmail.com>
2014-01-03 14:39:41 +0200

    Added detail view controller
    + controller can delete entities/save changes to storage

---
+++ PhotoGallery/PhotoDetailViewController.h
@@ -0,0 +1,32 @@
+//
+//  PhotoDetailViewController.h
+//  PhotoGallery
+//
+//  Created by Ankh on 02.01.14.
+//  Copyright (c) 2014 Ankh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Photo.h"
+
+@protocol PhotoDetailsControllerDelegate <NSObject>
+
+-(void)didDoneWithDetails:(id)sender andReload: (BOOL) reload;
+
+@end
+
+@interface PhotoDetailViewController : UIViewController <UITextViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UITextView *descriptionText;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+- (IBAction)actionSave:(id)sender;
+- (IBAction)actionDelete:(id)sender;
+- (IBAction)actionResignFirstResponder:(id)sender;
+
+@property (nonatomic, weak) id<PhotoDetailsControllerDelegate> delegate;
+@property (nonatomic, weak) Photo *photo;
+
+// interface method for parent view fontroller to setup this view with delegate & photo data
+-(void)setupBeforeShow:(id<PhotoDetailsControllerDelegate>)delegate withPhoto: (Photo *) photo;
+@end
---
+++ PhotoGallery/PhotoDetailViewController.m
@@ -0,0 +1,81 @@
+//
+//  PhotoDetailViewController.m
+//  PhotoGallery
+//
+//  Created by Ankh on 02.01.14.
+//  Copyright (c) 2014 Ankh. All rights reserved.
+//
+
+#import "PhotoDetailViewController.h"
+#import "PGUtils.h"
+
+@interface PhotoDetailViewController ()
+
+@end
+
+@implementation PhotoDetailViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // setup image & description controls
+    UIImage *image = [self.photo getImage];
+    [self.imageView setImage:image];
+    [self.imageView setNeedsDisplay];
+    [self.descriptionText setText:[self.photo metaDescription]];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // try to survive =D
+    [self.imageView setImage:nil];
+}
+
+#pragma mark - Main code
+
+
+// setup method
+-(void)setupBeforeShow:(id<PhotoDetailsControllerDelegate>)delegate withPhoto: (Photo *) photo {
+    self.delegate = delegate;
+    self.photo = photo;
+}
+
+// After user press "Done" button we must save changes to db and show master view.
+- (IBAction)actionSave:(id)sender {
+    [self.photo setMetaDescription:[self.descriptionText text]];
+    if (![PGUtils saveContext]) {
+        return;
+    }
+    
+    [self.delegate didDoneWithDetails:self andReload:YES];
+}
+
+// On button "Trash" pressed - delete photo and return to master view
+- (IBAction)actionDelete:(id)sender {
+    if (![self.photo deletePhoto])
+        return;
+    
+    [self.delegate didDoneWithDetails:self andReload:YES];
+}
+
+#pragma mark - UITextView Delegate & other UI staff
+
+// scroll view to show textview over keyboard
+- (void)textViewDidBeginEditing:(UITextView *)textView {
+    CGPoint scrollPoint = CGPointMake(0, textView.frame.origin.y);
+    [self.scrollView setContentOffset:scrollPoint animated:YES];
+}
+
+// scroll view down after keyboard was hidden
+- (void)textViewDidEndEditing:(UITextView *)textView {
+    [self.scrollView setContentOffset:CGPointZero animated:YES];
+}
+
+// When user touches outside of textview (on the transparrent button in background) - hide keyboard
+- (IBAction)actionResignFirstResponder:(id)sender {
+    [self.descriptionText resignFirstResponder];
+}
+
+@end

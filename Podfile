8232d3a87fabc7cb4c0341aa6547c71d2d3a702a
ankhzet <ankhzet@gmail.com>
2014-01-05 13:52:29 +0200

    Importing Kiwi with CocoaPods
    Kiwi framework added to "Test" build target

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '6.0'
+
+target :PhotoGalleryTests do
+	pod 'Kiwi'
+end
---
+++ Pods/Pods-PhotoGalleryTests-Kiwi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PhotoGalleryTests_Kiwi : NSObject
+@end
+@implementation PodsDummy_Pods_PhotoGalleryTests_Kiwi
+@end

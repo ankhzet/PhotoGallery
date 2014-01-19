44101c243d2d52533c1d4338d4f05ff174204cdf
ankhzet <ankhzet@gmail.com>
2014-01-19 17:03:04 +0200

    Removed useless "PhotoPickerViewController" & fixed some filters issues
    * photo-picking functionality moved to master controller
    * limited filters count to most usable
    * fixed filters duplication on filter-table reload
    
    Also:
    * fixed some view backgrounds
    * fixed unbound detail text view in details-view-controller

--- PhotoGallery/PGImageFilters.m
+++ PhotoGallery/PGImageFilters.m
@@ -17,145 +17,118 @@
 
 
 -(id)init {
-    if (!(self = [super init]))
-        return nil;
-    
-    self.filters = [NSMutableArray array];
-    self.applyedFilters = [NSMutableArray array];
-    [self prepareFilters];
-    return self;
+	if (!(self = [super init]))
+		return nil;
+	
+	self.filters = [NSMutableArray array];
+	self.applyedFilters = [NSMutableArray array];
+	[self prepareFilters];
+	return self;
 }
 
 -(NSUInteger) filtersCount {
-    return [self.filters count];
+	return [self.filters count];
 }
 
 -(void)prepareFilters {
-    NSArray *nonUsableFilters = @[
-    @"CIAdditionCompositing",
-    @"CIBlendWithMask",
-    @"CIColorMap",
-    @"CICopyMachineTransition",
-    @"CIDisintegrateWithMaskTransition",
-    @"CIFlashTransition",
-    @"CIMaximumCompositing",
-    @"CIMinimumCompositing",
-    @"CIModTransition",
-    @"CIMultiplyCompositing",
-    @"CISourceAtopCompositing",
-    @"CISourceInCompositing",
-    @"CISourceOutCompositing",
-    @"CISourceOverCompositing",
-    @"CISwipeTransition",
-    @"CICheckerboardGenerator",
-    @"CIConstantColorGenerator",
-    @"CIGaussianGradient",
-    @"CILinearGradient",
-    @"CIRadialGradient",
-    @"CIRandomGenerator",
-    @"CISmoothLinearGradient",
-    @"CIStarShineGenerator",
-    @"CIStripesGenerator"
-    ];
-    
-    NSArray *allNames = [CIFilter filterNamesInCategories:nil];
-    NSMutableArray *filteredNames = [NSMutableArray array];
-
-    for (NSString *filter in allNames)
-        if (([nonUsableFilters indexOfObject:filter] == NSNotFound) && ([filteredNames indexOfObject:filter] == NSNotFound))
-            if ([filter rangeOfString:@"Transition"].length <= 0)
-                [filteredNames addObject:filter];
-    
-    NSLog(@"Available filters: %i\n%@", [filteredNames count], filteredNames);
-    
-    for (NSString *filter in filteredNames) {
-            [self.filters addObject:[CIFilter filterWithName:filter]];
-    }
+	NSArray *usableFilters =
+	@[
+		@"CIPhotoEffectChrome",
+		@"CIPhotoEffectFade",
+		@"CIPhotoEffectInstant",
+		@"CIPhotoEffectMono",
+		@"CIPhotoEffectNoir",
+		@"CIPhotoEffectProcess",
+		@"CIPhotoEffectTonal",
+		@"CIPhotoEffectTransfer",
+		@"CIBloom",
+		@"CIColorInvert",
+		@"CIColorMonochrome",
+		@"CIColorPosterize",
+		@"CIFalseColor",
+		@"CIGaussianBlur",
+		@"CILinearToSRGBToneCurve",
+		@"CIPixellate"
+		];
+  
+	[self.filters removeAllObjects];
+	for (NSString *filter in usableFilters) {
+		[self.filters addObject:[CIFilter filterWithName:filter]];
+	}
 }
 
 -(CIImage *) processImage: (CIImage *) inputImage withFilter: (int) filterIndex {
-    // pick a filter
-    CIFilter *filter = [self.filters objectAtIndex:filterIndex];
-    if (!filter) {
-        return nil;
-    }
-    
-    @try {
-        // configure filter
-        [filter setValue:inputImage forKey:@"inputImage"];
-        
-//        // execute filter
-//        CGImageRef tempRef = [[CIContext contextWithOptions:nil]
-//                              createCGImage:filter.outputImage
-//                              fromRect:inputImage.extent];
-//        
-//        // convert output to certain image class
-//        CIImage *outputImage = [CIImage imageWithCGImage:tempRef];
-//        
-//        // free temp image
-//        CGImageRelease(tempRef);
-        
-        return filter.outputImage;//outputImage;
-    }
-    @catch (NSException *exception) {
-        NSLog(@"Unusable filter\n%@", [filter name]);
-    }
-    
-    return nil;
+	// pick a filter
+	CIFilter *filter = [self.filters objectAtIndex:filterIndex];
+	if (!filter) {
+		return nil;
+	}
+	
+	@try {
+		// configure filter
+		[filter setValue:inputImage forKey:@"inputImage"];
+		
+		return filter.outputImage;//outputImage;
+	}
+	@catch (NSException *exception) {
+		NSLog(@"Unusable filter\n%@", [filter name]);
+	}
+	
+	return nil;
 }
 
 +(CIImage *) makeCIImageFromUIImage: (UIImage *) sourceImage andFit: (CGSize) size {
-    CGSize source = [sourceImage size];
-    if (source.height <= 0 || source.width <= 0)
-        return nil;
-    
-    CGFloat ratio = source.width / source.height;
-    
-    if (source.width > size.width) { // fit by width
-        source.width = size.width;
-        source.height = source.width / ratio;
-    }
-    if (source.height > size.height) { // fit by height
-        source.height = size.height;
-        source.width = source.height * ratio;
-    }
-    
-    CGRect destRect = CGRectMake(0, 0, source.width,source.height);
-    
-    UIGraphicsBeginImageContext(source);
-    
-    CGContextFillRect(UIGraphicsGetCurrentContext(), destRect);
-    [sourceImage drawInRect:destRect];
-    
-    UIImage *tempImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    return [CIImage imageWithCGImage:[tempImage CGImage]];
+	CGSize source = [sourceImage size];
+	if (source.height <= 0 || source.width <= 0)
+		return nil;
+	
+	CGFloat ratio = source.width / source.height;
+	
+	if (source.width > size.width) { // fit by width
+		source.width = size.width;
+		source.height = source.width / ratio;
+	}
+	if (source.height > size.height) { // fit by height
+		source.height = size.height;
+		source.width = source.height * ratio;
+	}
+	
+	CGRect destRect = CGRectMake(0, 0, source.width,source.height);
+	
+	UIGraphicsBeginImageContext(source);
+	
+	CGContextFillRect(UIGraphicsGetCurrentContext(), destRect);
+	[sourceImage drawInRect:destRect];
+	
+	UIImage *tempImage = UIGraphicsGetImageFromCurrentImageContext();
+	UIGraphicsEndImageContext();
+	return [CIImage imageWithCGImage:[tempImage CGImage]];
 }
 
 -(void)startFilterGroup {
-    [self.applyedFilters removeAllObjects];
+	[self.applyedFilters removeAllObjects];
 }
 
 -(void)pushFilter: (int) filterIndex {
-    [self.applyedFilters addObject:[NSNumber numberWithInt:filterIndex]];
+	[self.applyedFilters addObject:[NSNumber numberWithInt:filterIndex]];
 }
 
 -(BOOL)popFilter {
-    BOOL hasLast = [self.applyedFilters count];
-    [self.applyedFilters removeLastObject];
-    return hasLast;
+	BOOL hasLast = [self.applyedFilters count];
+	[self.applyedFilters removeLastObject];
+	return hasLast;
 }
 
 -(BOOL)hasFiltersInGroup {
-    return [self.applyedFilters count];
+	return [self.applyedFilters count];
 }
 
 -(CIImage *)processImageWithFiltersGroup: (CIImage *) inputImage {
-    for (NSNumber *index in self.applyedFilters) {
-        inputImage = [self processImage:inputImage withFilter:[index integerValue]];
-    }
-    
-    return inputImage;
+	for (NSNumber *index in self.applyedFilters) {
+		inputImage = [self processImage:inputImage withFilter:[index integerValue]];
+	}
+	
+	return inputImage;
 }
 
 @end
--- PhotoGallery/PhotoGalleryViewController.m
+++ PhotoGallery/PhotoGalleryViewController.m
@@ -10,83 +10,132 @@
 
 #import "PreferencesViewController.h"
 #import "PhotoDetailViewController.h"
+#import "PhotoFiltersViewController.h"
 
 #import "PGDataProxyContainer.h"
 #import "ICloudEnabledStorage.h"
 
-@interface PhotoGalleryViewController () <PreferencessControllerDelegate, PhotoDetailsControllerDelegate>
+@interface PhotoGalleryViewController () <PreferencessControllerDelegate, PhotoDetailsControllerDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate>
 
+@property (nonatomic, strong) UIImage *pickedImage;
+
+- (IBAction)actionTakephoto:(id)sender;
 @end
 
 @implementation PhotoGalleryViewController
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-    
-    self.dataSource = [[PhotosTableDataSource alloc] init];
-    
-    [self.tableView setDataSource:self.dataSource];
-    [self.tableView setDelegate:self.dataSource];
-    
-    // when using ICloudEnabledStorage, must manualy subscribe for data updates notification,
-    // because ICloudEnabledStorage initializes storage in separate thread, and after first
-    // aquire of persistent storage data isn't actually loaded yet
-    id proxy = [[PGDataProxyContainer getInstance] dataProxy];
-    [proxy subscribeForUpdateNotifications:self selector:@selector(onCoreDataUpdate:)];
-    
-    // notification may be sended already
-    [self reloadTable];
+	[super viewDidLoad];
+  
+	self.dataSource = [[PhotosTableDataSource alloc] init];
+	
+	[self.tableView setDataSource:self.dataSource];
+	[self.tableView setDelegate:self.dataSource];
+	
+	// when using ICloudEnabledStorage, must manualy subscribe for data updates notification,
+	// because ICloudEnabledStorage initializes storage in separate thread, and after first
+	// aquire of persistent storage data isn't actually loaded yet
+	id proxy = [[PGDataProxyContainer getInstance] dataProxy];
+	[proxy subscribeForUpdateNotifications:self selector:@selector(onCoreDataUpdate:)];
+	
+	// notification may be sended already
+	[self reloadTable];
 }
 
 -(void)viewDidUnload {
-    [super viewDidUnload];
-    
-    // don't forget to unsubscribe and remove circular referencing...
-    id proxy = [[PGDataProxyContainer getInstance] dataProxy];
-    [proxy unSubscribeFromUpdateNotifications:self];
+	[super viewDidUnload];
+	
+	// don't forget to unsubscribe and remove circular referencing...
+	id proxy = [[PGDataProxyContainer getInstance] dataProxy];
+	[proxy unSubscribeFromUpdateNotifications:self];
 }
 
 - (void)didReceiveMemoryWarning
 {
-    [super didReceiveMemoryWarning];
+	[super didReceiveMemoryWarning];
 }
 
 // notification about coredata state update (on first load or iCloud import)
 - (void)onCoreDataUpdate:(NSNotification*)notification {
-    [self reloadTable];
+	[self reloadTable];
 }
 
 -(void)reloadTable {
-    [self.dataSource aquireData];
-    [self.tableView reloadData];
+	[self.dataSource aquireData];
+	[self.tableView reloadData];
 }
 
-#pragma mark Segues
+#pragma mark - Photo picker
+
+- (IBAction)actionTakephoto:(id)sender {
+	[self showPhotoPicker:self.view.frame];
+}
+
+// ImagePicker delegate.
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+	self.pickedImage = [info objectForKey:UIImagePickerControllerOriginalImage];
+	
+	[self dismissViewControllerAnimated:YES completion:nil];
+	[self performSegueWithIdentifier:@"showFilters" sender:self];
+}
+
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+	[self dismissViewControllerAnimated:YES completion:^{
+		//		[[self navigationController] popToRootViewControllerAnimated:YES];
+	}];
+}
+
+-(void) showPhotoPicker: (CGRect) popoverFrame {
+	UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+	
+	UIImagePickerControllerSourceType sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+	
+	if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+		sourceType = UIImagePickerControllerSourceTypeCamera;
+	}
+	
+	[picker setSourceType:sourceType];
+	
+	// Set up the other imagePicker properties
+	picker.allowsEditing = NO;
+	picker.delegate = self;
+	
+	[self presentViewController:picker animated:YES completion:nil];
+}
+
+#pragma mark - Segues
 
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    if ([[segue identifier]isEqualToString:@"showPreferences"]) {
-        UINavigationController *dvc = [segue destinationViewController];
-        PreferencesViewController *pvc = [dvc viewControllers][0];
-        pvc.delegate = self;
-    }
-
-    if ([[segue identifier]isEqualToString:@"showPhotoDetails"]) {
-        PhotoDetailViewController *dvc = [segue destinationViewController];
-        NSIndexPath *indexPath = [self.tableView indexPathForCell:sender];
-        [dvc setupBeforeShow:self withPhoto:[self.dataSource.photos objectAtIndex:indexPath.row]];
-    }
+	if ([[segue identifier]isEqualToString:@"showPreferences"]) {
+		UINavigationController *dvc = [segue destinationViewController];
+		PreferencesViewController *pvc = [dvc viewControllers][0];
+		pvc.delegate = self;
+	}
+	
+	if ([[segue identifier]isEqualToString:@"showPhotoDetails"]) {
+		PhotoDetailViewController *dvc = [segue destinationViewController];
+		NSIndexPath *indexPath = [self.tableView indexPathForCell:sender];
+		[dvc setupBeforeShow:self withPhoto:[self.dataSource.photos objectAtIndex:indexPath.row]];
+	}
+	
+	if ([[segue identifier] isEqualToString:@"showFilters"]) {
+		PhotoFiltersViewController *dvc = [segue destinationViewController];
+		[dvc setupBeforeShow:self.pickedImage];
+	}
 }
 
+#pragma mark - Delegate callbacks
+
 -(void)didDoneWithPreferences:(id)sender {
-    [sender dismissViewControllerAnimated:YES completion:NULL];
-    ((PreferencesViewController *)sender).delegate = NULL;
+	[sender dismissViewControllerAnimated:YES completion:NULL];
+	((PreferencesViewController *)sender).delegate = NULL;
 }
 
 -(void)didDoneWithDetails:(id)sender andReload: (BOOL) reload {
-    [self.navigationController popToViewController:self animated:YES];
-    ((PhotoDetailViewController *)sender).delegate = NULL;
-    [self reloadTable];
+	[self.navigationController popToViewController:self animated:YES];
+	((PhotoDetailViewController *)sender).delegate = NULL;
+	[self reloadTable];
 }
 
 @end
